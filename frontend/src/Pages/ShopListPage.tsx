743c4dfb61952ab0b998dbc232f1017fe3bad538
Aniket <aniketkumarg99@gmail.com>
2025-02-22 15:07:04 +0530

    added margin y to filter section

--- frontend/src/Pages/ShopListPage.tsx
+++ frontend/src/Pages/ShopListPage.tsx
@@ -6,11 +6,11 @@ const ShopListPage = () => {
   return (
     <>
       <Banner />
-      <div className="md:flex lg:w-[90%] max-w-7xl mx-auto">
+      <div className="md:flex lg:w-[90%] max-w-7xl my-16 mx-auto">
         <div className="bg-green-200 hidden lg:flex md:w-1/3 h-[60vh]">
             <FilterSection onFilter={(category) => console.log(category)} />
         </div>
-        <div className="bg-gray-100 w-full md:min-h-[40vh] my-16 min-h-[60vh]">
+        <div className="bg-gray-100 w-full md:min-h-[40vh] min-h-[60vh]">
           <h2 className="text-2xl font-semibold p-4">Products</h2>
           <ProductList />
         </div>
